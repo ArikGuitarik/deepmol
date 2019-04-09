ff08ac300cf636754445d8223489f2d1e2e539f6
ArikGuitarik <arik@elimelech.de>
2019-04-09 16:22:51 +0200

    Turned fully-connected neural network into a subclass of Model

--- model/fc_nn.py
+++ model/fc_nn.py
@@ -1,18 +1,31 @@
 import tensorflow as tf
+from .abstract_model import Model
 
 
-def fc_nn(x, layer_dims, activation=tf.nn.leaky_relu, output_activation=None):
+class FullyConnectedNN(Model):
     """A simple fully connected neural network
 
     Args:
-        x: input tensor
         layer_dims: list specifying the dimensions of hidden layers and output
         activation: activation function to apply after each hidden layer
         output_activation: activation function to apply to the output
     """
-    for hidden_dim in layer_dims[:-1]:
-        x = tf.layers.dense(x, hidden_dim, activation)
 
-    output_tensor = tf.layers.dense(x, layer_dims[-1], output_activation)
+    def __init__(self, hparams, layer_dims, activation=tf.nn.leaky_relu, output_activation=None):
+        super(FullyConnectedNN, self).__init__(hparams)
+        self.layer_dims = layer_dims
+        self.activation = activation
+        self.output_activation = output_activation
 
-    return output_tensor
+    def _forward(self, x):
+        """Forward pass of the network.
+
+        Args:
+            x: input tensor
+        """
+        for hidden_dim in self.layer_dims[:-1]:
+            x = tf.layers.dense(x, hidden_dim, self.activation)
+
+        output_tensor = tf.layers.dense(x, self.layer_dims[-1], self.output_activation)
+
+        return output_tensor
