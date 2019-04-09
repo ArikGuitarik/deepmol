dc66c7336f9e7c2382336368f5d18bde7a5b6878
ArikGuitarik <arik@elimelech.de>
2019-04-09 19:22:54 +0200

    Renamed filter_matrix to matrix_filters for clarity.

--- model/message_passing.py
+++ model/message_passing.py
@@ -22,31 +22,31 @@ class ConvFilterGenerator(Model):
         """Forward pass of filter generator.
 
         :param distance_matrix: distance matrix shaped [batch_size, num_atoms, num_atoms]
-        :return: generated convolution filters. If hparams.use_matrix_filter is True, the shape is
+        :return: generated convolution filters. If hparams.use_matrix_filters is True, the shape is
             [batch_size, num_atoms, num_atoms, hidden_state_dim, hidden_state_dim],
             else we have a vector for each atom pair: [batch_size, num_atoms, num_atoms, hidden_state_dim]
         """
         batch_size = tf.shape(distance_matrix)[0]
         num_atoms = tf.shape(distance_matrix)[1]
         hidden_state_dim = self.hparams.hidden_state_dim
-        use_matrix_filter = self.hparams.use_matrix_filter
+        use_matrix_filters = self.hparams.use_matrix_filters
 
         # map every distance value in the whole batch separately:
         dist_mat_reshaped = tf.reshape(distance_matrix, [batch_size, num_atoms, num_atoms, 1], name="dist_mat_reshaped")
 
         layer_dims = np.ones(self.hparams.filter_hidden_layers + 1) * self.hparams.filter_hidden_dim
-        layer_dims[-1] = hidden_state_dim ** 2 if use_matrix_filter else hidden_state_dim  # output dim
+        layer_dims[-1] = hidden_state_dim ** 2 if use_matrix_filters else hidden_state_dim  # output dim
         activation = tf.nn.leaky_relu if self.hparams.use_leaky_relu else tf.nn.relu
         fc_nn = FullyConnectedNN(self.hparams, layer_dims, activation, output_activation=None)
         conv_filter = fc_nn.forward(dist_mat_reshaped)
 
-        if use_matrix_filter:
+        if use_matrix_filters:
             conv_filter = tf.reshape(conv_filter,
                                      [batch_size, num_atoms, num_atoms, hidden_state_dim, hidden_state_dim],
-                                     name='filter_matrix')
+                                     name='matrix_filters')
         else:
             conv_filter = tf.reshape(conv_filter, [batch_size, num_atoms, num_atoms, hidden_state_dim],
-                                     name='filter_vector')
+                                     name='vector_filters')
 
         return conv_filter
 
@@ -63,11 +63,11 @@ class MatrixMessagePassing(Model):
     def __init__(self, hparams):
         super(MatrixMessagePassing, self).__init__(hparams)
 
-    def _forward(self, hidden_states, filter_matrix):
+    def _forward(self, hidden_states, matrix_filters):
         """Forward pass for generating messages using matrix filters.
 
         :param hidden_states: Hidden states of all atoms, shaped [batch_size, num_atoms, hidden_state_dim]
-        :param filter_matrix: generated convolution filters, shaped
+        :param matrix_filters: generated convolution filters, shaped
             [batch_size, num_atoms, num_atoms, hidden_state_dim, hidden_state_dim]
         :return: sum of incoming messages to each atom, shaped [batch_size, num_atoms, hidden_state_dim]
         """
@@ -78,11 +78,11 @@ class MatrixMessagePassing(Model):
         # multiply matrix with hidden states and add bias to generate messages
         hidden_states_flat = tf.reshape(hidden_states, [batch_size, num_atoms * hidden_state_dim, 1],
                                         name='hidden_states_flat')
-        filter_matrix = tf.transpose(filter_matrix, [0, 1, 3, 2, 4])
-        filter_matrix = tf.reshape(filter_matrix,
-                                   [batch_size, num_atoms * hidden_state_dim, num_atoms * hidden_state_dim],
-                                   name='filter_matrix_flat')
-        messages = tf.matmul(filter_matrix, hidden_states_flat)  # (b, n*d, n*d) x (b, n*d, 1) -> (b, n*d, 1)
+        matrix_filters = tf.transpose(matrix_filters, [0, 1, 3, 2, 4])
+        matrix_filters = tf.reshape(matrix_filters,
+                                    [batch_size, num_atoms * hidden_state_dim, num_atoms * hidden_state_dim],
+                                    name='matrix_filters_flat')
+        messages = tf.matmul(matrix_filters, hidden_states_flat)  # (b, n*d, n*d) x (b, n*d, 1) -> (b, n*d, 1)
         messages = tf.reshape(messages, [batch_size * num_atoms, hidden_state_dim], name='messages_unbiased')
         messages += tf.get_variable("message_bias", shape=hidden_state_dim)
         messages = tf.reshape(messages, [batch_size, num_atoms, hidden_state_dim], name='messages')
