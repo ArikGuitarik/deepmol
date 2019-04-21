09c42b7bc235d077a5b9017b1350b4ea43f7d403
ArikGuitarik <arik@elimelech.de>
2019-04-21 22:24:53 +0200

    Added test for ConfigReader

--- tests/test_train_util.py
+++ tests/test_train_util.py
@@ -1,6 +1,8 @@
 import unittest
 import numpy.testing as npt
-from train_util import CurveSmoother
+import os
+from train_util import CurveSmoother, ConfigReader
+import tensorflow as tf
 
 
 class TestCurveSmoother(unittest.TestCase):
@@ -26,6 +28,47 @@ class TestCurveSmoother(unittest.TestCase):
         smoother = CurveSmoother(1)
         smoothed_data = [smoother.smooth(val) for val in data]
         npt.assert_allclose(smoothed_data, [data[0] for _ in data])
+
+
+class TestConfigReader(unittest.TestCase):
+    def test_reader(self):
+        hparams_1 = tf.contrib.training.HParams(x=2, y=3, z=True)
+        hparams_2 = tf.contrib.training.HParams(x=2.3, y=3, z=True)
+        hparams_3 = tf.contrib.training.HParams(x=3.14, y=42, z=False)
+        hparams_default = tf.contrib.training.HParams(x=1.8, y=2.9, z=False)
+
+        tmpdir = 'unittest_configs'
+        tmpfile_1 = os.path.join(tmpdir, '1.json')
+        tmpfile_2 = os.path.join(tmpdir, '2.json')
+        tmpfile_3 = os.path.join(tmpdir, '3.json')
+        try:
+            os.makedirs(tmpdir, exist_ok=True)
+            with open(tmpfile_1, 'w') as f:
+                f.write(hparams_1.to_json())
+            with open(tmpfile_2, 'w') as f:
+                f.write(hparams_2.to_json())
+
+            config_reader = ConfigReader(tmpdir, hparams_default)
+            new_configs = config_reader.get_new_hparam_configs()
+            self.assertEqual(len(new_configs), 2)
+            self.assertDictEqual(hparams_1.values(), new_configs['1'].values())
+            self.assertDictEqual(hparams_2.values(), new_configs['2'].values())
+
+            new_configs = config_reader.get_new_hparam_configs()
+            self.assertEqual(len(new_configs), 0)
+
+            os.remove(tmpfile_1)
+
+            with open(tmpfile_3, 'w') as f:
+                f.write(hparams_3.to_json())
+
+            new_configs = config_reader.get_new_hparam_configs()
+            self.assertEqual(len(new_configs), 1)
+            self.assertDictEqual(hparams_3.values(), new_configs['3'].values())
+        finally:
+            os.remove(tmpfile_2)
+            os.remove(tmpfile_3)
+            os.rmdir(tmpdir)
 
 
 if __name__ == '__main__':
