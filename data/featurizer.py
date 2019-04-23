7721617e86f4ac822f1b672a5552834f2bd70db7
ArikGuitarik <arik@elimelech.de>
2019-04-23 15:29:24 +0200

    New featurizer + Added NoneType in atom features to replace mask

--- data/featurizer.py
+++ data/featurizer.py
@@ -7,14 +7,12 @@ class FeaturizedMolecule:
 
     :param atom_features: atom representations of dimension [num_atoms, num_atom_features]
     :param interaction_matrix: matrix of atomic interactions shaped [num_atoms, num_atoms, num_interaction_features]
-    :param mask: vector of length num_atoms indicating whether an atom is actually present (1) or zero-padded (0).
     :param coordinates: xyz-coordinates of the atoms, shaped [num_atoms, 3]
     """
 
-    def __init__(self, atom_features, interaction_matrix, mask, coordinates=None):
+    def __init__(self, atom_features, interaction_matrix, coordinates=None):
         self.atom_features = atom_features
         self.interaction_matrix = interaction_matrix
-        self.mask = mask
         self.coordinates = coordinates
 
 
@@ -25,9 +23,22 @@ class Featurizer:
     :param implicit_hydrogen: True if hydrogen atoms should be treated implicitly.
     """
 
-    def __init__(self, max_num_atoms, implicit_hydrogen=False):
+    def __init__(self, max_num_atoms, implicit_hydrogen=True):
         self.max_num_atoms = max_num_atoms
         self.implicit_hydrogen = implicit_hydrogen
+        # defined in the sub class:
+        self._num_atom_features = None
+        self._num_interaction_features = None
+
+    @property
+    def num_atom_features(self):
+        """Number of features encoding each atom."""
+        return self._num_atom_features
+
+    @property
+    def num_interaction_features(self):
+        """Number of features encoding the interactions between atoms."""
+        return self._num_interaction_features
 
     def featurize(self, rdkit_mol, shuffle_atoms=False):
         """Convert an rdkit_mol to a FeaturizedMolecule.
@@ -48,18 +59,14 @@ class Featurizer:
 
         atom_features = self._generate_atom_features(rdkit_mol)
         interaction_matrix = self._generate_interaction_matrix(rdkit_mol)
-        mask = self._generate_mask(rdkit_mol)
         coordinates = self._generate_coordinates(rdkit_mol)
 
-        return FeaturizedMolecule(atom_features, interaction_matrix, mask, coordinates)
+        return FeaturizedMolecule(atom_features, interaction_matrix, coordinates)
 
     def _generate_atom_features(self, rdkit_mol):
         raise NotImplementedError('Featurizer must be implemented in child class')
 
     def _generate_interaction_matrix(self, rdkit_mol):
-        raise NotImplementedError('Featurizer must be implemented in child class')
-
-    def _generate_mask(self, rdkit_mol):
         raise NotImplementedError('Featurizer must be implemented in child class')
 
     def _generate_coordinates(self, rdkit_mol):
@@ -79,9 +86,16 @@ class Featurizer:
 
 
 class DistanceFeaturizer(Featurizer):
-    """Featurizer based on interatomic distance."""
-    num_atom_features = 5
-    num_interaction_features = 1
+    """Featurizer based on interatomic distance.
+
+    Atom features: one-hot-encoded types [H, C, O, N, F, none/padded], H is only present if implicit_hydrogen=False
+    Interaction features: Euclidian distance matrix
+    """
+
+    def __init__(self, max_num_atoms, implicit_hydrogen=True):
+        super().__init__(max_num_atoms, implicit_hydrogen)
+        self._num_atom_features = 5 if implicit_hydrogen else 6
+        self._num_interaction_features = 1
 
     def _generate_interaction_matrix(self, rdkit_mol):
         """Generate interaction matrix using the real-valued Euclidian distance as interaction feature."""
@@ -96,30 +110,55 @@ class DistanceFeaturizer(Featurizer):
         return interaction_matrix
 
     def _generate_atom_features(self, rdkit_mol):
-        """Generate atom features by one-hot encoding the atom types.
-
-        If hydrogen is implicit, the entry for hydrogen is replaced by the number of implicit hydrogens.
-        """
+        """Generate atom features by one-hot encoding the atom types."""
         atoms = rdkit_mol.GetAtoms()
-        atomic_numbers = np.array([1, 6, 7, 8, 9])  # QM9 data set only has HCNOF
+        if self.implicit_hydrogen:
+            atomic_numbers = np.array([6, 7, 8, 9])  # QM9 data set with implicit hydrogen only contains CNOF
+        else:
+            atomic_numbers = np.array([1, 6, 7, 8, 9])  # QM9 data set only contains HCNOF
 
-        atom_features = np.zeros((len(atoms), self.num_atom_features))
+        atom_features = np.zeros((len(atoms), len(atomic_numbers) + 1))
         for i, atom in enumerate(atoms):
             atomic_num = atom.GetAtomicNum()
-            atom_type_one_hot = (atomic_numbers == atomic_num).astype(int)  # HCNOF one-hot
-            atom_features[i, 0:5] = atom_type_one_hot
-            # if hydrogen is implicit, the 1st entry is always 0 -> replace it with number of implicit hydrogen atoms
-            if self.implicit_hydrogen:
-                atom_features[i, 0] = atom.GetNumImplicitHs()
+            atom_type_one_hot = (atomic_numbers == atomic_num).astype(int)
+            atom_features[i, 0:len(atomic_numbers)] = atom_type_one_hot
+
         # add zero padding
         padding_size = self.max_num_atoms - len(atoms)
         atom_features = np.pad(atom_features, ((0, padding_size), (0, 0)), mode='constant')
 
+        # set none-type for padded atoms
+        is_padded = 1 - np.sum(atom_features, axis=1)
+        atom_features[:, len(atomic_numbers)] = is_padded
+
         return atom_features
 
-    def _generate_mask(self, rdkit_mol):
-        num_atoms = rdkit_mol.GetNumAtoms()
-        mask = np.zeros(self.max_num_atoms)
-        mask[:num_atoms] = 1
 
-        return mask
+class DistanceNumHFeaturizer(DistanceFeaturizer):
+    """Same as DistanceFeaturizer, but adds information about the number of implicit hydrogens at each atom.
+
+    If hydrogen is implicit, the entry for hydrogen in the one-hot vector is replaced by the number of
+    implicit hydrogens at the atom.
+    """
+
+    def __init__(self, max_num_atoms, implicit_hydrogen=True):
+        super().__init__(max_num_atoms, implicit_hydrogen)
+        self._num_atom_features = 6
+        self._num_interaction_features = 1
+
+    def _generate_atom_features(self, rdkit_mol):
+        """Generate atom features by one-hot encoding the atom types.
+
+        If hydrogen is implicit, the entry for hydrogen is replaced by the number of implicit hydrogens.
+        """
+        atom_features = super()._generate_atom_features(rdkit_mol)
+
+        if self.implicit_hydrogen:
+            # add implicit hydrogens to existent atoms
+            implicit_hydrogens = np.zeros(self.max_num_atoms)
+            for i, atom in enumerate(rdkit_mol.GetAtoms()):
+                implicit_hydrogens[i] = atom.GetNumImplicitHs()
+
+            atom_features = np.concatenate((implicit_hydrogens[:, np.newaxis], atom_features), axis=1)
+
+        return atom_features
--- tests/test_featurizer.py
+++ tests/test_featurizer.py
@@ -3,7 +3,7 @@ import unittest
 import rdkit.Chem as Chem
 import os
 import numpy as np
-from data.featurizer import DistanceFeaturizer
+from data.featurizer import DistanceFeaturizer, DistanceNumHFeaturizer
 
 max_num_atoms, max_num_atoms_h = 9, 29  # implicit hydrogen, explicit hydrogen
 
@@ -15,13 +15,13 @@ class TestDistanceFeaturizer(unittest.TestCase):
 
         mol_supplier = Chem.SDMolSupplier(sdf_path, removeHs=True)  # import molecules from sdf file
         self.rdkit_mol = mol_supplier[9]
-        distance_featurizer = DistanceFeaturizer(max_num_atoms, implicit_hydrogen=True)
-        self.feat_mol = distance_featurizer.featurize(self.rdkit_mol)
+        self.featurizer = DistanceFeaturizer(max_num_atoms, implicit_hydrogen=True)
+        self.feat_mol = self.featurizer.featurize(self.rdkit_mol)
 
         mol_supplier = Chem.SDMolSupplier(sdf_path, removeHs=False)  # explicit hydrogen
         rdkit_mol_h = mol_supplier[9]
-        distance_featurizer_h = DistanceFeaturizer(max_num_atoms_h, implicit_hydrogen=False)
-        self.feat_mol_h = distance_featurizer_h.featurize(rdkit_mol_h)
+        self.featurizer_h = DistanceFeaturizer(max_num_atoms_h, implicit_hydrogen=False)
+        self.feat_mol_h = self.featurizer_h.featurize(rdkit_mol_h)
 
     def test_max_num_atoms(self):
         distance_featurizer_fail = DistanceFeaturizer(max_num_atoms=1, implicit_hydrogen=True)
@@ -29,36 +29,76 @@ class TestDistanceFeaturizer(unittest.TestCase):
             distance_featurizer_fail.featurize(self.rdkit_mol)
 
     def test_atom_features(self):
-        desired = np.zeros([max_num_atoms, DistanceFeaturizer.num_atom_features])
-        desired[0, 0:2] = 3, 1
-        desired[1, 1], desired[2, 2] = 1, 1
+        desired = np.zeros([max_num_atoms, self.featurizer.num_atom_features])
+        desired[0:2, 0] = [1, 1]  # CC
+        desired[2, 1] = 1  # N
+        desired[3:, -1] = 1  # none/padded
         npt.assert_equal(self.feat_mol.atom_features, desired)
 
-        desired_h = np.zeros([max_num_atoms_h, DistanceFeaturizer.num_atom_features])
-        desired_h[3:6, 0] = 1
-        desired_h[0:2, 1] = 1
-        desired_h[2, 2] = 1
+        desired_h = np.zeros([max_num_atoms_h, self.featurizer_h.num_atom_features])
+        desired_h[0:2, 1] = 1  # CC
+        desired_h[2, 2] = 1  # N
+        desired_h[3:6, 0] = 1  # H
+        desired_h[6:, -1] = 1  # none/padded
         npt.assert_equal(self.feat_mol_h.atom_features, desired_h)
 
     def test_interaction_features(self):
-        desired = np.zeros([max_num_atoms, max_num_atoms, DistanceFeaturizer.num_interaction_features])
+        desired = np.zeros([max_num_atoms, max_num_atoms, self.featurizer.num_interaction_features])
         desired[0, 1:3, 0] = 1.45685382, 2.61188178
         desired[1, 0:3, 0] = 1.45685382, 0, 1.15502801
         desired[2, 0:2, 0] = 2.61188178, 1.15502801
 
         npt.assert_allclose(self.feat_mol.interaction_matrix, desired)
 
-        desired_shape_h = [max_num_atoms_h, max_num_atoms_h, DistanceFeaturizer.num_interaction_features]
+        desired_shape_h = [max_num_atoms_h, max_num_atoms_h, self.featurizer_h.num_interaction_features]
         npt.assert_equal(self.feat_mol_h.interaction_matrix.shape, desired_shape_h)
 
-    def test_mask(self):
-        desired = np.zeros([max_num_atoms])
-        desired[0:3] = 1
-        npt.assert_equal(self.feat_mol.mask, desired)
 
-        desired_h = np.zeros([max_num_atoms_h])
-        desired_h[0:6] = 1
-        npt.assert_equal(self.feat_mol_h.mask, desired_h)
+class TestDistanceNumHFeaturizer(unittest.TestCase):
+    def setUp(self):
+        data_import_directory = os.path.dirname(__file__)
+        sdf_path = os.path.join(data_import_directory, '100_sample_molecules.sdf')
+
+        mol_supplier = Chem.SDMolSupplier(sdf_path, removeHs=True)  # import molecules from sdf file
+        self.rdkit_mol = mol_supplier[9]
+        self.featurizer = DistanceNumHFeaturizer(max_num_atoms, implicit_hydrogen=True)
+        self.feat_mol = self.featurizer.featurize(self.rdkit_mol)
+
+        mol_supplier = Chem.SDMolSupplier(sdf_path, removeHs=False)  # explicit hydrogen
+        rdkit_mol_h = mol_supplier[9]
+        self.featurizer_h = DistanceNumHFeaturizer(max_num_atoms_h, implicit_hydrogen=False)
+        self.feat_mol_h = self.featurizer_h.featurize(rdkit_mol_h)
+
+    def test_max_num_atoms(self):
+        distance_featurizer_fail = DistanceNumHFeaturizer(max_num_atoms=1, implicit_hydrogen=True)
+        with self.assertRaises(ValueError):
+            distance_featurizer_fail.featurize(self.rdkit_mol)
+
+    def test_atom_features(self):
+        desired = np.zeros([max_num_atoms, self.featurizer.num_atom_features])
+        desired[0, 0] = 3  # implicit H
+        desired[0:2, 1] = 1  # CC
+        desired[2, 2] = 1  # N
+        desired[3:, -1] = 1  # none/padded
+        npt.assert_equal(self.feat_mol.atom_features, desired)
+
+        desired_h = np.zeros([max_num_atoms_h, self.featurizer_h.num_atom_features])
+        desired_h[0:2, 1] = 1  # CC
+        desired_h[2, 2] = 1  # N
+        desired_h[3:6, 0] = 1  # H
+        desired_h[6:, -1] = 1  # none/padded
+        npt.assert_equal(self.feat_mol_h.atom_features, desired_h)
+
+    def test_interaction_features(self):
+        desired = np.zeros([max_num_atoms, max_num_atoms, self.featurizer.num_interaction_features])
+        desired[0, 1:3, 0] = 1.45685382, 2.61188178
+        desired[1, 0:3, 0] = 1.45685382, 0, 1.15502801
+        desired[2, 0:2, 0] = 2.61188178, 1.15502801
+
+        npt.assert_allclose(self.feat_mol.interaction_matrix, desired)
+
+        desired_shape_h = [max_num_atoms_h, max_num_atoms_h, self.featurizer_h.num_interaction_features]
+        npt.assert_equal(self.feat_mol_h.interaction_matrix.shape, desired_shape_h)
 
 
 if __name__ == '__main__':
