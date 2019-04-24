2d000611131f23d43185f703589e9d7cad6d14ff
ArikGuitarik <arik@elimelech.de>
2019-04-24 21:08:17 +0200

    Added evaluation of atom and smiles accuracy.

--- train_vae.py
+++ train_vae.py
@@ -4,6 +4,7 @@ import argparse
 from train_util import QM9Trainer, ConfigReader
 from model.molvae import MolVAE
 from data.featurizer import DistanceFeaturizer
+from data.molecules import NPMol
 
 logging.basicConfig(format='%(levelname)s - %(message)s', level=logging.INFO)
 
@@ -43,14 +44,67 @@ class VAETrainer(QM9Trainer):
         return val_loss
 
     def _eval_results(self):
-        """Compute loss on validation and test set and write to results file."""
+        """Compute scores on validation and test set and write to results file."""
         results = {}
         results['val_loss'] = self._average_over_dataset(self._val_iterator, self._val_loss)
         results['test_loss'] = self._average_over_dataset(self._test_iterator, self._test_loss)
 
+        logging.info('Computing validation accuracies...')
+        val_accuracies = self._compute_accuracies(self._val_iterator, self._val_mols, self._val_mols_rec)
+        results['val_atom_acc'], results['val_smiles_acc'] = val_accuracies
+
+        logging.info('Computing test accuracies...')
+        test_accuracies = self._compute_accuracies(self._test_iterator, self._test_mols, self._test_mols_rec)
+        results['test_atom_acc'], results['test_smiles_acc'] = test_accuracies
+
         self._write_eval_results_to_file(results)
 
+    def _compute_accuracies(self, data_iterator, mols, mols_rec):
+        """Iterate over the given set to evaluate scores.
+
+        :param data_iterator: The initializable_iterator of the relevant data set.
+        :param mols: TFMolBatch of the original molecules from the data set.
+        :param mols_rec: TFMolBatch of the reconstructed molecules.
+        :return:
+            - smiles_accuracy: The ratio of molecules where the SMILES string was correctly reconstructed.
+            - atom_accuracy: The ratio of molecules where all atom types were correctly reconstructed.
+        """
+        self._sess.run(data_iterator.initializer)
+        correct_smiles_counter, correct_atoms_counter, total_counter = 0, 0, 0
+        while True:
+            try:
+                if self._coordinate_output:
+                    atoms, coords, atoms_rec, coords_rec = self._sess.run(
+                        [mols.atoms, mols.coordinates, mols_rec.atoms, mols_rec.coordinates])
+                    mols_np = NPMol.create_from_batch(atoms, batch_coordinates=coords)
+                    rec_mols_np = NPMol.create_from_batch(atoms_rec, batch_coordinates=coords_rec)
+                else:
+                    atoms, dist, atoms_rec, dist_rec = self._sess.run(
+                        [mols.atoms, mols.distances, mols_rec.atoms, mols_rec.distances])
+                    mols_np = NPMol.create_from_batch(atoms, batch_distances=dist)
+                    rec_mols_np = NPMol.create_from_batch(atoms_rec, batch_distances=dist_rec)
+
+                for mol, rec_mol in zip(mols_np, rec_mols_np):
+                    if mol.smiles == rec_mol.smiles:
+                        correct_smiles_counter += 1
+                    if mol.atoms == rec_mol.atoms:
+                        correct_atoms_counter += 1
+                    total_counter += 1
+            except tf.errors.OutOfRangeError:
+                break
+        smiles_accuracy = 0
+        atom_accuracy = 0
+        if total_counter != 0:
+            smiles_accuracy = correct_smiles_counter / total_counter
+        if correct_atoms_counter != 0:
+            atom_accuracy = correct_atoms_counter / total_counter
+        return atom_accuracy, smiles_accuracy
+
     def _build_model(self, hparams):
+        """Build the VAE model given the hyperparameter configuration. Overrides superclass method.
+
+        :param hparams: tf.contrib.training.HParams object
+        """
         vae = MolVAE(hparams, self.featurizer.max_num_atoms, self.featurizer.num_atom_features)
         train_loss = vae.calculate_loss(self._train_mols)
         # TensorBoard Summaries
@@ -65,6 +119,11 @@ class VAETrainer(QM9Trainer):
 
         self._val_loss = vae.calculate_loss(self._val_mols)
         self._test_loss = vae.calculate_loss(self._test_mols)
+
+        self._val_mols_rec = vae.reconstruct(self._val_mols)
+        self._test_mols_rec = vae.reconstruct(self._test_mols)
+
+        self._coordinate_output = hparams.coordinate_output
 
 
 if __name__ == '__main__':
